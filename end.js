31711db158036afd272f7dfd10aeeae0373558ba
dean <d.lewis.rave@gmail.com>
2022-05-18 19:03:56 +0100

    saves 5 scores in highest to lowest order

--- end.js
+++ end.js
@@ -15,13 +15,15 @@ saveHighScore = e => {
   e.preventDefault();
 
   const score = {
-    score: mostRecentScore,
+    score: Math.floor(Math.random() * 100),
     name: username.value
   };
   highScores.push(score);
   highScores.sort((a,b) => b.score - a.score)
 
   highScores.splice(5)
-  console.log(highScores);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign('/');
 };
 
