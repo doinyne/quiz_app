7dac7ec12eb8f92f7bcb0d2ac18d5ca349feebbd
dean <d.lewis.rave@gmail.com>
2022-05-23 11:43:01 +0100

    pulling questions from api appears on quiz app

--- game.js
+++ game.js
@@ -18,8 +18,21 @@ fetch("https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=
   })
   .then(loadedQuestions => {
     console.log(loadedQuestions.results);
-    //questions = loadedQuestions;
-    //startGame();
+    questions = loadedQuestions.results.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
+
+      const answerChoices = [ ... loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+      answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index+1)] = choice;
+      })
+      return formattedQuestion
+    });
+    startGame();
   })
   .catch(err => {
     console.error(err);
